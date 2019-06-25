425a00b3f36299a3a4a03ef3a492dc2a3cc11853
mapleFU <1506118561@qq.com>
2019-06-25 23:26:06 +0800

    [Update] Finish basic logic for login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,22 @@
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetMQ;
+using NetMQ.Sockets;
+using org.apache.zookeeper;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OSApiInterface.Services;
 
 namespace OSApiInterface.Controllers
 {
@@ -13,20 +28,45 @@ namespace OSApiInterface.Controllers
     
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController
+    public class UserController: ControllerBase
     {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
         
         [Route("login")]
-        public async void Login()
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserInput userInput)
         {
-            throw new NotImplementedException();
+            var usr = await _userService.FindUserBymail(userInput.Email);
+            if (usr == null)
+            {
+                
+                return NotFound("User with email not found");
+            }
+
+            string s = await _userService.Login();
+            if (s == null)
+            {
+                return BadRequest("User password error.");
+            }
+
+            return Ok(s);
         }
 
         [Route("register")]
-        public async void Register([FromBody] UserInput userInput)
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserInput userInput)
         {
-            
-            throw new NotImplementedException();
+
+            if (await _userService.ExistsUserWithEmail(userInput.Email))
+            {
+                return BadRequest("user with email already exists");
+            }
+
+            return Ok(_userService.Register(userInput.Email, userInput.Password));
         }
     }
 }
